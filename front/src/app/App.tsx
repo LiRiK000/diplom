faca0a179d399f0edb0c0d8134db7e9eec3a09e2
Fedor <fedorbitaev45@gmail.com>
2025-06-08 19:16:58 +0300

    UPD[add tab feedback ]

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -66,7 +66,7 @@ function App() {
               <Route
                 path="/admin"
                 element={
-                  <ProtectedRoute adminOnly>
+                  <ProtectedRoute >
                     <AdminPage />
                   </ProtectedRoute>
                 }
