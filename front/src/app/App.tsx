6e48e1a51b4061fdddc5eb0de69c30d44d860f06
Fedor <fedorbitaev45@gmail.com>
2025-06-08 19:34:31 +0300

    UPD[fix]

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -66,7 +66,7 @@ function App() {
               <Route
                 path="/admin"
                 element={
-                  <ProtectedRoute >
+                  <ProtectedRoute adminOnly>
                     <AdminPage />
                   </ProtectedRoute>
                 }
